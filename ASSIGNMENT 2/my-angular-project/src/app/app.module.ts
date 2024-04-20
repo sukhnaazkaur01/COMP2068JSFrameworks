df1ed95e76edf8822f485c3e7e7650e27f036d2f
sukhnaazkaur01 <sukhnaaz007@gmail.com>
2024-04-20 15:02:14 -0400

    Final exam

---
+++ ASSIGNMENT 2/my-angular-project/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentService } from './services/student.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    StudentService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ASSIGNMENT 2/my-angular-project/src/app/students/student.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Student } from '../models/student.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+  private apiUrl = 'http://example.com/api/students';
+
+  constructor(private http: HttpClient) {}
+
+  getAllStudents(): Observable<Student[]> {
+    return this.http.get<Student[]>(this.apiUrl);
+  }
+
+  getStudentById(id: number): Observable<Student> {
+    return this.http.get<Student>(`${this.apiUrl}/${id}`);
+  }
+
+  addStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(this.apiUrl, student);
+  }
+
+  updateStudent(student: Student): Observable<Student> {
+    return this.http.put<Student>(`${this.apiUrl}/${student.id}`, student);
+  }
+
+  deleteStudent(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
